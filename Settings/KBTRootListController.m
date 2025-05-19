a7255081b12a7eb42902124226cd561dd0745615
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-19 18:22:28 +0800

    Update KBTRootListController.m

--- Settings/KBTRootListController.m
+++ Settings/KBTRootListController.m
@@ -1,13 +1,14 @@
 #import "KBTRootListController.h"
+#import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
 
 @implementation KBTRootListController
 
 - (NSArray *)specifiers {
-    if (!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
-    }
-    return _specifiers;
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+	return _specifiers;
 }
 
 @end
