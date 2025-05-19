801b20f0c8b5d58e6ba8566fd733e4c365c1dbe0
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-19 18:22:06 +0800

    Update KBTRootListController.m

--- Settings/KBTRootListController.m
+++ Settings/KBTRootListController.m
@@ -1,14 +1,13 @@
 #import "KBTRootListController.h"
-#import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
-
 
 @implementation KBTRootListController
 
 - (NSArray *)specifiers {
     if (!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"KeyboardTheme" target:self];
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
     }
     return _specifiers;
 }
+
 @end
