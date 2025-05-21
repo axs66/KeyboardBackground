43c12d95c49aab4aae6bfcc8f6f327d8aaf8e59d
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-21 13:44:43 +0800

    Update KBTRootListController.m

--- Settings/KBTRootListController.m
+++ Settings/KBTRootListController.m
@@ -1,19 +1,29 @@
-#import "KBTRootListController.h"
+#import <Cephei/HBPreferences.h>
+#import <CepheiUI/HBListController.h>
 #import <spawn.h>
+
+@interface KBTRootListController : HBListController
+@end
 
 @implementation KBTRootListController
 
-- (NSArray *)specifiers {
-    if (!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"KeyboardTheme" target:self];
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        // 加载 Settings/KeyboardTheme.plist
+        [self loadFromSpecifierPlistName:@"KeyboardTheme"];
     }
-    return _specifiers;
+    return self;
 }
 
 - (void)respring {
     pid_t pid;
     const char *args[] = {"killall", "-9", "SpringBoard", NULL};
     posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)args, NULL);
+}
+
+- (void)reloadTheme {
+    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("com.axs66.KeyboardTheme/ReloadTheme"), NULL, NULL, true);
 }
 
 @end
