2d7af464b5b7de7fb47e76b860056a313ae89fa2
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-21 12:57:09 +0800

    Update KBTRootListController.m

--- Settings/KBTRootListController.m
+++ Settings/KBTRootListController.m
@@ -1,38 +1,25 @@
 #import "KBTRootListController.h"
 #import <Preferences/PSSpecifier.h>
+#import "../Settings/ThemeManager.h"
 
 @implementation KBTRootListController
 
 - (NSArray *)specifiers {
     if (!_specifiers) {
-        _specifiers = [[self loadSpecifiersFromPlistName:@"KeyboardTheme" target:self] retain];
-
-        PSSpecifier *themePicker = [PSSpecifier preferenceSpecifierNamed:@"当前主题"
-                                                                     target:self
-                                                                        set:@selector(setPreferenceValue:specifier:)
-                                                                        get:@selector(readPreferenceValue:)
-                                                                     detail:nil
-                                                                       cell:PSPopupCell
-                                                                       edit:nil];
-        [themePicker setProperty:@"ThemeName" forKey:@"key"];
-        [themePicker setValues:@[@"default", @"dark"] titles:@[@"默认", @"深色"]];
-        [_specifiers addObject:themePicker];
+        _specifiers = [self loadSpecifiersFromPlistName:@"KeyboardTheme" target:self];
     }
     return _specifiers;
 }
 
-- (id)readPreferenceValue:(PSSpecifier *)specifier {
-    NSDictionary *settings = [NSDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/com.yourcompany.keyboardtheme.plist"];
-    return settings[specifier.properties[@"key"]] ?: @"default";
+- (void)respring {
+    pid_t pid;
+    const char *args[] = {"killall", "-9", "SpringBoard", NULL};
+    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)args, NULL);
 }
 
-- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
-    NSMutableDictionary *settings = [NSMutableDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/com.yourcompany.keyboardtheme.plist"] ?: [NSMutableDictionary dictionary];
-    settings[specifier.properties[@"key"]] = value;
-    [settings writeToFile:@"/var/mobile/Library/Preferences/com.yourcompany.keyboardtheme.plist" atomically:YES];
-
-    // 通知热重载
-    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("com.yourcompany.keyboardtheme/reload"), NULL, NULL, YES);
+- (void)reloadTheme {
+    [[ThemeManager sharedManager] reloadConfig];
+    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("com.axs66.KeyboardTheme/ReloadTheme"), NULL, NULL, true);
 }
 
 @end
