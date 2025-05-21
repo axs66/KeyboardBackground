32d4f66fff244b6748295182c1f456a702e03d8d
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-21 12:54:34 +0800

    Update ThemeManager.h

--- Settings/ThemeManager.h
+++ Settings/ThemeManager.h
@@ -1,7 +1,17 @@
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface ThemeManager : NSObject
-@property (nonatomic, strong) NSDictionary *currentTheme;
+
+@property (nonatomic, strong) NSDictionary *themeConfig;
+
 + (instancetype)sharedManager;
-- (void)loadTheme;
+- (void)loadThemeWithName:(NSString *)themeFileName;
+- (UIColor *)colorForKey:(NSString *)key;
+- (NSString *)stringForKey:(NSString *)key;
+
 @end
+
+NS_ASSUME_NONNULL_END
